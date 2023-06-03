9cf20cdab7a1d5003eeb73885113d215dcf92553
Wiktor Chmielewski <chmielewski.wiktor@gmail.com>
2023-06-03 14:40:59 +0200

    x

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-# USAGE: bash build.sh <flashcards/learn/rewards/users/ui>
+# USAGE: bash build.sh <flashcards/learn/rewards/users/ui> <github name>
 
 NAME=$1
+USER=$2
 
 docker build -t $NAME ./$NAME/ &&
-docker image tag $NAME:latest ghcr.io/wiktoor/$NAME:latest &&
-docker image push ghcr.io/wiktoor/$NAME:latest
+docker image tag $NAME:latest ghcr.io/$USER/$NAME:latest &&
+docker image push ghcr.io/$USER/$NAME:latest
