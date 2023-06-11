8c7146c39711b8a17286372a0045aeee1cb2b19a
Maciek <mmioduszewski4@gmail.com>
2023-06-11 21:39:33 +0200

    added log

--- learn/src/main/java/pl/mimuw/learn/LearnController.java
+++ learn/src/main/java/pl/mimuw/learn/LearnController.java
@@ -1,6 +1,7 @@
 package pl.mimuw.learn;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/")
+@Slf4j
 public class LearnController {
     private final LearnService learnService;
 
@@ -16,6 +18,7 @@ public class LearnController {
             @RequestParam String userId,
             @RequestParam List<String> flashcardsToLearn
     ) {
+        log.info("/learn endpoint was called");
         return learnService.flashcardsInLearnOrder(userId, flashcardsToLearn);
     }
 }
